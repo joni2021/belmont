53dc49d668938396deb96f32e1f1e954b2b0e914
jonatan jorge <jonh_gabriel05@hotmail.com>
2019-07-04 16:08:16 -0300

    alta y editar prestamos con sistema frances funcionando

--- public/js/calcular_cuotas.js
+++ public/js/calcular_cuotas.js
@@ -1,8 +1,14 @@
 $(document).ready(function () {
+    calcular();
 
     var cuota, tasa, monto, porcentajeCuota;
 
-    $("#dues,#amount").on("change keyup", function (ev) {
+    $("#dues,#amount").on("change keyup ", calcular);
+
+
+
+
+    function calcular (ev) {
         var dues = $("#dues");
         cuota = parseFloat($(dues).find("option:selected").data("due"));
         // tasa = parseFloat(ev.currentTarget.selectedOptions[0].dataset.porcent);
@@ -14,52 +20,71 @@ $(document).ready(function () {
         }
 
 
-        porcentajeCuota = (tasa / 100);
-        porcentajeCuota = parseFloat(porcentajeCuota.toFixed(3));
+        porcentajeCuota = (tasa / 100)/12;
+        porcentajeCuota = parseFloat(porcentajeCuota);
 
-        var valCuota = monto * (( porcentajeCuota * (Math.pow( 1 + porcentajeCuota, cuota ))) / (( (Math.pow( 1 + porcentajeCuota, cuota )) - 1 )));
+        var valCuota = monto * (porcentajeCuota / (1 - Math.pow(1 + porcentajeCuota,((-1) * cuota))))
+        // var valCuota = monto * (( porcentajeCuota * (Math.pow( 1 + porcentajeCuota, cuota ))) / (( (Math.pow( 1 + porcentajeCuota, cuota )) - 1 )));
 
         $("#tasa").val(tasa);
         $("#dues_amount").val(parseFloat(valCuota).toFixed(2));
 
         var tasaPrimerCuota = parseFloat($("#dues option")[0].dataset.porcent)
 
-        var tabla = "<tr><td>1</td><td>" + tasaPrimerCuota + "%</td><td>$" + calcular_cuota(2).toLocaleString() + "</td>";
-        // var tabla;
+        // var tabla = "<tr><td>1</td><td>" + tasaPrimerCuota + "%</td><td>$" + calcular_cuota(2).toLocaleString() + "</td>";
+        var tabla;
 
         // var pagoTotal = 0;
-        var pagoTotal = parseFloat(calcular_cuota(cuota));
+        // var pagoTotal = parseFloat(calcular_cuota(cuota));
 
-         for(var i = 2; i <= cuota; i++ ){
-             var tasa = $("#dues option[value=" + i + "]").data("porcent");
-             console.log(i)
-             console.log($("#dues option[value=" + i + "]"))
+        var interesPagado = 0.0000;
+        var amortizacionPagado = 0.0000;
+        var valorDeudaASaldar = monto;
 
-             tabla += "<tr><td>" + i + "</td>";
-             tabla += "<td>" + parseFloat(tasa) + "%</td>";
-             tabla += "<td>$" + calcular_cuota(i).toLocaleString() + "</td></tr>";
 
-             pagoTotal += parseFloat(calcular_cuota(i)) //+ parseFloat(pagoTotal) ;
-         }
+        for(var i = 0; i < cuota; i++ ){
+            if(i < 2){
+                var tasa = $("#dues option[value=" + 2 + "]").data("porcent");
+            }else{
+                var tasa = $("#dues option[value=" + i + "]").data("porcent");
+            }
 
-        tabla += "<tr>";
+            interesPagado = valorDeudaASaldar * porcentajeCuota;
+            amortizacionPagado = valCuota - interesPagado;
+            valorDeudaASaldar = valorDeudaASaldar - amortizacionPagado;
+
+            var ind = i+1;
+            tabla += "<tr><td>" + ind + "</td>";
+            tabla += "<td>" + parseFloat(tasa) + "%</td>";
+            tabla += "<td>$" + parseFloat(interesPagado.toFixed(2)) + "</td>";
+            tabla += "<td>$" + parseFloat(amortizacionPagado.toFixed(2)) + "</td>";
+            tabla += "<td>$" + parseFloat(valorDeudaASaldar.toFixed(2)) + "</td></tr>";
+
+            // pagoTotal += parseFloat(calcular_cuota(ind)).toFixed(2) //+ parseFloat(pagoTotal) ;
+
+        }
+
+        // tabla += "<tr>";
 
 
         $(".datosCuota").empty();
         $(".datosCuota").append(tabla);
-        $("#precioTotal").text("$ " + pagoTotal.toLocaleString())
+        // $("#precioTotal").text("$ " + pagoTotal)
 
 
         $(".tablaCuotas").removeClass("d-none");
-        
-    })
+
+    }
+
 
 
     function calcular_cuota(cuota){
 
-        var valor =  monto * (( porcentajeCuota * (Math.pow( 1 + porcentajeCuota, cuota ))) / (( (Math.pow( 1 + porcentajeCuota, cuota )) - 1 )))
+        var valor =  monto * (porcentajeCuota / (1 - Math.pow(1 + porcentajeCuota,((-1) * cuota))))
 
-        return parseFloat(valor).toFixed(2);
+        // var valor =  monto * (( porcentajeCuota * (Math.pow( 1 + porcentajeCuota, cuota ))) / (( (Math.pow( 1 + porcentajeCuota, cuota )) - 1 )))
+
+        return parseFloat(valor);
     }
 
 })
--- resources/views/forms/partials/dues_table.blade.php
+++ resources/views/forms/partials/dues_table.blade.php
@@ -4,31 +4,14 @@
         <tr>
             <th>Cuota</th>
             <th>Tasa</th>
-            <th>Importe mensual($)</th>
+            <th>Interés pagado($)</th>
+            <th>Amortización($)</th>
+            <th>Saldo a pagar($)</th>
         </tr>
         </thead>
         <tbody class="datosCuota">
-            @if(!empty($model))
-                {{--<tr>--}}
-                    {{--<td>1</td>--}}
-                    {{--<td>{{ $financing->first()->porcent }}%</td>--}}
-                    {{--<td>${{ number_format($model->monthlyAmount($financing->first()->porcent,2),2) }}</td>--}}
-                {{--</tr>--}}
-                @foreach($financing as $f)
-                    <tr>
-                        <td>{{ $f->due }}</td>
-                        <td>{{ $f->porcent }}%</td>
-                        <td class="montoMensual">${{ number_format($model->monthlyAmount($f->porcent,$f->due),2) }}</td>
-                    </tr>
-                    @break($f->due == $model->dues)
-                @endforeach
-            @endif
+
         </tbody>
-        <tfooter>
-            <tr>
-                <td colspan="2" class="text-right">Total</td>
-                <td class="text-danger" id="precioTotal">$ @if(!empty($model)) {{ number_format($model->TotalAmount,2) }} @endif</td>
-            </tr>
-        </tfooter>
+
     </table>
 </div>
