78364fa140348207862ca7c3e591d4d41d00b12d
jonatan jorge <jonh_gabriel05@hotmail.com>
2019-08-06 16:11:23 -0300

    migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -24,7 +24,7 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->tinyInteger('company_id',false,true);
+            $table->tinyInteger('company_id',false,true)->default(1);
 
             $table->foreign('dni_type_id')->references('id')->on('dni_types');
             $table->foreign('company_id')->references('id')->on('companies');
